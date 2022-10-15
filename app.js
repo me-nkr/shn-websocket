39e8bc60e16ecf7ad12cddb947769b423a38329a
Naveen K R <thenaveenkr7@gmail.com>
2022-10-15 20:35:38 +0530

    add route for style.css

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { Server: Socket } = require('socket.io');
 const http = require('http');
+const { addHandlers } = require('./src')
 
 const app = express();
 const server = http.createServer(app);
@@ -12,6 +13,14 @@ app.get('/', (req, res) => {
 
 app.get('/app.js', (req, res) => {
     res.sendFile(__dirname + '/public/app.js')
+})
+
+app.get('/style.css', (req, res) => {
+    res.sendFile(__dirname + '/public/style.css')
+})
+
+io.on('connection', (socket) => {
+    addHandlers(io, socket);
 })
 
 app.listen(3000);
